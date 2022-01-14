dbc36c86106be2471095333fbf8613707d908656
carlos-augusto-roque <carlos@ofsolution.com>
2022-01-14 14:16:45 -0300

    .

--- demo-security/demo-security/src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ demo-security/demo-security/src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -1,13 +1,37 @@
 package com.mballem.curso.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mballem.curso.security.service.EmailService;
+
 @SpringBootApplication
-public class DemoSecurityApplication {
+public class DemoSecurityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DemoSecurityApplication.class, args);
-		//System.out.println(new BCryptPasswordEncoder().encode("123456")); 	
+			
+	}
+	
+//	@Autowired
+//	JavaMailSender sender;
+	
+	@Autowired
+	EmailService service;
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+//		SimpleMailMessage simple = new SimpleMailMessage();
+//		simple.setTo("carlosagustoroque@gmail.com");
+//		simple.setText("Teste numero 1");
+//		simple.setSubject("teste 1");	
+//		sender.send(simple);
+		
+		service.enviarPedidoDeConfirmacaoDeCadastro("carlosagustoroque@gmail.com","9852pol");
 	}
 }
